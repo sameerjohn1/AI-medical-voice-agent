cdc873b4dbed8ae335afbfdd692a17707ce46430
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-12-01 10:23:26 +0500

    neondb postgresql attached

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { motion } from "motion/react";
-import { FeatureBentoGrid } from "./_component/FeatureBentoGrid";
 
-export default function Home() {
+import { motion } from "motion/react";
+
+export default function HeroSectionOne() {
   return (
-    <div className="relative  my-10 flex  flex-col items-center justify-center">
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -18,7 +18,7 @@ export default function Home() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Revolutionize Pateint Care wit AI Voice Agents"
+          {"Launch your website in hours, not days"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -49,9 +49,9 @@ export default function Home() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-          Deliver instant, accurate medical assisntance through natural voice
-          conversations. Automate appointments scheduling, symptiom triange, and
-          follow-up care-24/7.
+          With AI, you can launch your website in hours, not days. Try our best
+          in class, state of the art, cutting edge AI tools to get your website
+          up.
         </motion.p>
         <motion.div
           initial={{
@@ -99,8 +99,6 @@ export default function Home() {
           </div>
         </motion.div>
       </div>
-
-      <FeatureBentoGrid />
     </div>
   );
 }
