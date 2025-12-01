9f026547d509c39828e3398a1db4a606296cbf8c
sameerjohn1 <sameerprogrammer5@gmail.com>
2025-12-01 14:06:27 +0500

    create api for user

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+"use client";
+import { useEffect } from "react";
+import axios from "axios";
+import { useUser } from "@clerk/nextjs";
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user]);
+
+  const CreateNewUser = async () => {
+    const result = await axios.post("/api/users");
+    console.log(result.data);
+  };
+
   return <div>{children}</div>;
 }
 
